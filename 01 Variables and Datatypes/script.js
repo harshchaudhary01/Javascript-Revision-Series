29f697d2e955805d69a39668fdd82d1c7225b964
harshchaudhary01 <harshcareer01@gmail.com>
2026-01-06 19:31:46 +0530

    Day-01

---
+++ 01 Variables and Datatypes/script.js
@@ -0,0 +1,13 @@
+// Example 1.)
+
+const product = {
+    name: "Parker Jotter Standard CT Ball Pen (Black)",
+    rating: 4,
+    numberOfRating: 7002,
+    isDeal: true,
+    discountPrice: 270,
+    mrp: 285,
+    discount: 5
+};
+console.log(product)
+console.log(typeof product["isDeal"]);
